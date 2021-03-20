76e341756226d3d3372c1477a0a42a3c3f0d5604
earlyXbirdNG <pw.id+gh@icloud.com>
2021-03-21 00:56:26 +0100

    bild

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $num = mysqli_num_rows($result);
 <div class="wrapper">
 
 <a href="index.php" class="button_home">Startseite</a>
-<br><br>
+<br><br><br><br>
 
 <img src="img/sbahn.png" alt="Banner: RRX">
 

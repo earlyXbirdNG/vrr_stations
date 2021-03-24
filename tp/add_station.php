abacc5057c1fc69c478550e768859879fa62a02e
earlyXbirdNG <pw.id+gh@icloud.com>
2021-03-24 12:18:06 +0100

    Added Ability to show reserved stations in section "Offen"

---
+++ tp/add_station.php
@@ -0,0 +1,88 @@
+<?php
+
+// Datenbankverbindung öffnen
+require 'secret_webinterface.php';
+
+// Fehler abfangen
+if (!$dbconnect) {
+    die("Die Verbindung zur Datenbank konnte nicht hergestellt werden. Bitte wende dich an einen Administrator zur weiteren Unterstzützung.");
+}
+
+
+if (isset($_POST["submit"])) {
+    // mysqli_real_escape_string() sorgt dafür, dass eingegebener Text nicht als SQL-Befehl
+    // verstanden werden kann (z.B. Anführungszeichen koennten sonst für Probleme sorgen)
+    $id = mysqli_real_escape_string($dbconnect, $_POST["id"]);
+    $status = mysqli_real_escape_string($dbconnect, $_POST["status"]);
+    $nutzer = mysqli_real_escape_string($dbconnect, htmlentities($_SERVER["HTTP_X_USER"]));
+
+
+    // SQL-Abfrage definieren
+    $sql = "UPDATE stations SET status='" . $status . "',nutzer='" . $nutzer . "',datum=default WHERE id='" . $id . "'";
+
+    // Abfrage durchfuehren
+    $result = mysqli_query($dbconnect, $sql);
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Stations - internetz.app</title>
+</head>
+<body>
+<div class="wrapper">
+    
+<a href="index.php" class="button_home">Startseite</a>
+<br><br>
+<br><br>
+
+<img src="img/sbahn.png" alt="Banner: RRX">
+
+
+<!--  <a href="admin_login.php" class="button_home">Administrieren</a>  -->
+    <main>
+    
+        <h1>Ergebnis Deiner Anfrage</h1>
+
+        <p>
+            <b>
+                <?php
+
+                // Pruefen, ob direkt oder durch Formular abgerufen
+                if (!isset($_POST["submit"])) {
+                    echo "Unbekannter Fehler (120).<br>" .
+                        "Gehe zurück und versuche es erneut Wenn der Fehler wiederholt auftritt,<br>" .
+                        "wende dich bitte an den Support.<br>";
+                }
+                // Durch Formular, aber keine Daten angegeben
+                elseif (!isset($_POST["submit"])) {
+                    echo "Leeres Formular (121).<br>" .
+                    "Gehe zurück und versuche es erneut Wenn der Fehler wiederholt auftritt,<br>" .
+                    "wende dich bitte an den Support.<br>";
+                }
+                // Pruefen ob  Erfolg
+                elseif (isset($result) && $result) {
+                    echo "Dein Eintrag <b>" . $status . "</b> wurde vermerkt.";
+                }
+                // SQL-Fehler
+                else {
+                    echo "Huch, ein Fehler. Das sollte nicht passieren. Fehlercode: <code>" . mysqli_error($dbconnect) . "</code><br>" .
+                        "Gehe zurück und versuche es erneut Wenn der Fehler wiederholt auftritt,<br>" .
+                        "wende dich bitte an den Support.<br>";
+                }
+
+                ?>
+            </b>
+        </p>
+
+        <a href="index.php" class="button">Stationsübersicht</a>
+        <a href="edit_station.php" class="button button-green">Neue Meldung</a>
+    </main>
+</div>
+</body>
+</html>
--- tp/stationmaster.php
+++ tp/stationmaster.php
@@ -5,7 +5,7 @@ require 'secret_webinterface.php';
 
 // Fehler abfangen
 if (!$dbconnect) {
-    die("Die Verbindung zur Datenbank konnte nicht hergestellt werden. Bitte wende dich an einen Administrator zur weiteren Unterstzuetzung.");
+    die("Die Verbindung zur Datenbank konnte nicht hergestellt werden. Bitte wende dich an einen Administrator zur weiteren Unterstzützung.");
 }
 
 // SQL-Abfrage definieren
@@ -33,21 +33,23 @@ $num = mysqli_num_rows($result);
 <body>
 <div class="wrapper">
 
+
 <a href="index.php" class="button_home">Startseite</a>
 <br><br>
 <br><br>
 
 <img src="img/sbahn.png" alt="Banner: RRX">
 
+
 <!-- <a href="admin_login.php" class="button_home">Administrieren</a> -->
     <main>
-       <!-- <img src="img/banner_grillparty.jpg" alt="Banner: Grillparty"> -->
-        <h1>Station aktualisieren</h1>
+        <h1>Admin-Center</h1>
+        
         <p>
-            Waehle die Station die du aktualisieren moechtest.
-        </p>
-      <!--  <p><strong>Platzhater fuer einen fetten Text</strong></p> -->
-      <form action="update.php" method="post">
+            Wähle die Station die du aktualisieren möchtest.
+
+
+            <form action="update.php" method="post">
             
             <label for='id'>Station:</label><br>
             <select id="id" name="id" placeholder="Station" required>
@@ -56,6 +58,7 @@ $num = mysqli_num_rows($result);
 
                     while ($dataset = mysqli_fetch_assoc($result)) {
                         echo "<option value=" . htmlentities($dataset["id"]) . ">" . htmlentities($dataset["station"]) . "</option>";
+                      //  echo "<option value=" . htmlentities($dataset["id"]) . ">" . htmlentities($dataset["station"]) . " (" . htmlentities($dataset["status"]) . ")" . "</option>";
                     }                
                 }
             ?>
@@ -65,16 +68,22 @@ $num = mysqli_num_rows($result);
                 <label for="status">Status:</label><br>
                 <select id="status" name="status" placeholder="Neuer Status" required>
                     <option value="Erhoben">Erhoben</option>
+                    <option value="Reserviert">Reserviert</option>
                     <option value="Offen">Offen</option>
                     </select>
                 <br><br>
 
-                <label for="nutzer">Deine Initalien:</label><br>
-                <input type="text" id="nutzer" name="nutzer" placeholder="z.B. VRR" required>
+                <label for="nutzer">Nutzer:</label><br>
+                <input disabled required type="text" id="nutzer" name="nutzer" value="<?php echo htmlentities($_SERVER["HTTP_X_USER"]); ?>">
                 <br><br>
 
                 <input type="submit" id="submit" name="submit" value="Senden" class="button">
         </form>
+
+
+        </p>
+      <!--  <p><strong>Platzhater fuer einen fetten Text</strong></p> -->
+
     </main>
 </div>
 </body>

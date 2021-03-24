1e803673db9fa2d8432a6aaf5e00bc86338ea3b7
earlyXbirdNG <pw.id+gh@icloud.com>
2021-03-24 12:29:46 +0100

    Design

--- offen.php
+++ offen.php
@@ -49,7 +49,7 @@ $num_reserved = mysqli_num_rows($result_count_reserved);
         <h1>Stationsübersicht</h1>
 
         <?php
-        echo "<h3>(Offen: " . htmlentities($num) . " Stationen)</h3>";
+        echo "<h3>(Offen: " . htmlentities($num) . " Stationen, beinhaltet )" . htmlentities($num_reserved) . "reservierte Stationen </h3>";
         echo "<h3>(Davon Reserviert: " . htmlentities($num_reserved) . " Stationen)</h3>";
         ?>
 
